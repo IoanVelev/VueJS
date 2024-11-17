cb6bcfb307afae40f1dad65255a7c6870b8b9a2f
Ioan Velev <ioanvelev46@gmail.com>
2024-11-17 18:29:38 +0200

    Update vue-starter-project

--- 01.Intro/01.vue-starter-project/src/App.vue
+++ 01.Intro/01.vue-starter-project/src/App.vue
@@ -1,13 +1,36 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      counter: 0
+    }
   },
-};
+  methods: {
+    increment(){
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
+  }
+}
 </script>
 
 <template>
-  <HelloWorld />
+  <h1 class="title">Hello world</h1>
+  <p class="counter">Counter: {{ counter }}</p>
+  <button class="btn" @click="increment">Increment</button>
+  <button class="btn" @click="decrement">Decrement</button>
 </template>
+
+<style>
+.title, .counter {
+  text-align: center;
+}
+
+.btn {
+  display: flex;
+  margin-top: 40px;
+  justify-self: center;
+}
+</style>
