115325437d3d7d17b3b396393752dac950a2e32c
Ioan Velev <ioanvelev46@gmail.com>
2024-11-18 23:36:19 +0200

    Update app

--- 02. Rendering and Styles/lecture-examples/src/components/HelloWorld.vue
+++ 02. Rendering and Styles/lecture-examples/src/components/HelloWorld.vue
@@ -1,44 +1,46 @@
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+  msg: String,
+});
+
+const count = ref(0);
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://v2.vuejs.org/">Vue 2</a>.
-    </h3>
+  <div class="container" style="text-align:center">
+    <img src="../assets/vue.svg" alt="Logo" style="width: 10rem; margin: 1rem auto;">
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+      <button type="button" @click="count++">
+        count is {{ count }}
+      </button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
+    <p>
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+      starter
+    </p>
+    <p>
+      Install
+      <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+      in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">
+      Click on the Vite and Vue logos to learn more
+    </p>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    display: block;
-    text-align: left;
-  }
+.read-the-docs {
+  color: #888;
 }
 </style>
--- 02. Rendering and Styles/lecture-examples/src/main.js
+++ 02. Rendering and Styles/lecture-examples/src/main.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './styles/reset.css';
+import App from './App.vue';
 
-import './assets/main.css'
-
-new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+const app = createApp(App);
+app.mount('#app');
