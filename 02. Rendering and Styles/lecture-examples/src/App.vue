73e4b3bad6e4eb1853ef2617c9d98d17af53d242
Ioan Velev <ioanvelev46@gmail.com>
2024-11-19 16:50:28 +0200

    Add click event

--- 02. Rendering and Styles/lecture-examples/src/App.vue
+++ 02. Rendering and Styles/lecture-examples/src/App.vue
@@ -1,10 +1,27 @@
 <script>
 export default {
-
+  data() {
+    return {
+      isDay: true,
+    };
+  },
+  methods: {
+    onClick() {
+      this.isDay = !this.isDay;
+    },
+  },
 };
 </script>
 
 <template>
   <h1>Hello world</h1>
-  <h2>It's sunny outside</h2>
+  <h2 v-if="isDay">
+    It's sunny outside! &#127774;
+  </h2>
+  <h2 v-else>
+    It's dark outside! &#x1F311;
+  </h2>
+  <button @click="onClick">
+    Toggle
+  </button>
 </template>
