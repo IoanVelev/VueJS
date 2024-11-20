b24533ec81e2770bca26ec7a5ebbba656fbd40e8
Ioan Velev <ioanvelev46@gmail.com>
2024-11-20 22:43:00 +0200

    Test event modifier

--- 02. Rendering and Styles/lecture-examples/src/App.vue
+++ 02. Rendering and Styles/lecture-examples/src/App.vue
@@ -76,13 +76,18 @@ export default {
     onClick() {
       this.isDay = !this.isDay;
     },
-    changeTitleText(arg) {
+    changeTitleText(arg, eventObj) {
+      console.log('Change title text - ', eventObj);
+
       if (this.title === 'Hello world') {
         this.title = arg;
       }
       else {
         this.title = 'Hello world';
       }
+    },
+    clickEventModifier() {
+      console.log('Clicked event modifier');
     },
   },
 };
@@ -114,14 +119,17 @@ export default {
   </ul>
 
   <div>
-    <h4>Books: </h4>
+    <h4>Books:</h4>
     <p v-for="(book, key, indx) in bookInfo" :key="indx">
       {{ ` ${key}: ${book} ` }}
     </p>
   </div>
 
-  <button class="titleChanger" @click="changeTitleText('Goodbye')">
+  <button class="titleChanger" @click="changeTitleText('Goodbye', $event)">
     Change title
+  </button>
+  <button @click.ctrl.exact="clickEventModifier">
+    Event modifiers
   </button>
 </template>
 
@@ -132,7 +140,7 @@ export default {
 }
 
 .originalTitle {
-  color: green
+  color: green;
 }
 
 .changedTitle {
