be6b9eda0e190097c99e211dc1cdd4a81f6a6e42
Ioan Velev <ioanvelev46@gmail.com>
2024-11-20 19:55:31 +0200

    Use v-for directive for list rendering

--- 02. Rendering and Styles/lecture-examples/src/App.vue
+++ 02. Rendering and Styles/lecture-examples/src/App.vue
@@ -3,6 +3,32 @@ export default {
   data() {
     return {
       isDay: true,
+      cartProducts: [
+        {
+          id: 1,
+          name: 'Smartphone',
+          price: 499.99,
+          quantity: 2,
+        },
+        {
+          id: 2,
+          name: 'Laptop',
+          price: 999.99,
+          quantity: 1,
+        },
+        {
+          id: 3,
+          name: 'Headphones',
+          price: 79.99,
+          quantity: 3,
+        },
+        {
+          id: 4,
+          name: 'Tablet',
+          price: 299.99,
+          quantity: 2,
+        },
+      ],
     };
   },
   methods: {
@@ -27,4 +53,12 @@ export default {
   <h3 v-show="!isDay">
     This text is shown when it's not a day.
   </h3>
+  <h3>Products: </h3>
+  <br>
+  <br>
+  <ul>
+    <li v-for="product in cartProducts" :key="product.id">
+      {{ product.name }}
+    </li>
+  </ul>
 </template>
