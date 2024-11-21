4fd839a1583251e823903cda2ff74c3dc7cc5c2f
Ioan Velev <ioanvelev46@gmail.com>
2024-11-21 17:14:28 +0200

    Create timer app

---
+++ 02. Rendering and Styles/timer-app/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      userInput: '',
+      time: 0
+    }
+  },
+  methods: {
+    onInput(event) {
+      this.userInput = event.target.value
+    },
+    onStart() {
+      // TODO
+    },
+    onPause() {
+      //TODO
+    },
+    onRestart() {
+      // TODO
+    }
+  }
+
+}
+</script>
+
+<template>
+ <h1 class="title">Timer app</h1>
+ <input type="text" @input="onInput">
+ <button type="button">Start</button>
+ <button type="button">Pause</button>
+ <button type="button">Reset</button>
+</template>
+
+<style scoped>
+.title {
+ font-family:Arial, Helvetica, sans-serif
+}
+</style>
