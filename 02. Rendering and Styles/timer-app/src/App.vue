45d5a14e15b8eaad5d78970b6d3f14ffe6689081
Ioan Velev <ioanvelev46@gmail.com>
2024-11-21 17:51:30 +0200

    Update App.vue

--- 02. Rendering and Styles/timer-app/src/App.vue
+++ 02. Rendering and Styles/timer-app/src/App.vue
@@ -2,38 +2,56 @@
 export default {
   data() {
     return {
-      userInput: '',
-      time: 0
-    }
+      userInput: "",
+      timerSeconds: 0,
+    };
   },
   methods: {
     onInput(event) {
-      this.userInput = event.target.value
+      this.userInput = event.target.value;
+    },
+    timerHandler() {
+      const interval = setInterval(() => {
+        if (this.timerSeconds > 0) {
+          this.timerSeconds -= 1;
+        } else {
+          clearInterval(interval);
+        }
+      }, 1000);
     },
     onStart() {
-      // TODO
+      const [h, m, s] = this.userInput.split(":").map((num) => Number(num));
+      this.timerSeconds = s + 60 * m + 3600 * h;
+      this.timerHandler();
     },
     onPause() {
       //TODO
     },
-    onRestart() {
-      // TODO
-    }
-  }
-
-}
+    onReset() {
+      this.timerSeconds = 0;
+      this.userInput = "";
+    },
+  },
+};
 </script>
 
 <template>
- <h1 class="title">Timer app</h1>
- <input type="text" @input="onInput">
- <button type="button">Start</button>
- <button type="button">Pause</button>
- <button type="button">Reset</button>
+  <h1 class="title">Timer app</h1>
+  <input type="text" @input="onInput" />
+  <button type="button" @click="onStart">Start</button>
+  <button type="button" @click="onPause">Pause</button>
+  <button type="button" @click="onReset">Reset</button>
+  <p class="time-remaining" v-if="timerSeconds">
+    Remaining time: {{ timerSeconds }}
+  </p>
 </template>
 
 <style scoped>
 .title {
- font-family:Arial, Helvetica, sans-serif
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.time-remaining {
+  font-size: 20px;
 }
 </style>
