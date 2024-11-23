a9de9886a766dd90fb214dbd4b7e3e46b8c332c8
Ioan Velev <ioanvelev46@gmail.com>
2024-11-23 23:46:39 +0200

    Add default text for each slot

--- 03. Components/lecture-examples/src/components/CustomLayout.vue
+++ 03. Components/lecture-examples/src/components/CustomLayout.vue
@@ -1,9 +1,19 @@
 <template>
   <article>
+    <slot name="header">
+      <header>
+        <h1>Default slot header</h1>
+      </header>
+    </slot>
     <section>
         <slot>
-          This is a default slot's value
+          <h1>This is a default slot's value</h1>
         </slot>
     </section>
+    <slot name="footer">
+      <footer>
+        <i>Default slot footer</i>
+      </footer>
+    </slot>
   </article>
 </template>
