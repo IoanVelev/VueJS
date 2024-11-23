adeb19b95d24769cf7c0dfd381eba740277c0446
Ioan Velev <ioanvelev46@gmail.com>
2024-11-23 17:48:04 +0200

    Create ButtonCounter component

--- 03. Components/lecture-examples/src/App.vue
+++ 03. Components/lecture-examples/src/App.vue
@@ -1,47 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import ButtonCounter from './components/ButtonCounter.vue';
+
+export default {
+  components: {
+    ButtonCounter,
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="title">Components example</h1>
+  <ButtonCounter />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.title {
+  display: grid;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 40px;
+  height: 3vh;
 }
 </style>
---
+++ 03. Components/lecture-examples/src/components/ButtonCounter.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    onClick() {
+      this.count += 1;
+    },
+  },
+};
+</script>
+
+<template>
+  <p class="text-counter">I was clicked {{ count }} times.</p>
+  <button class="btn-counter" @click="onClick">Click me</button>
+</template>
+
+<style>
+.text-counter {
+  display: flex;
+  justify-content: left;
+  position: fixed;
+  margin-top: 100px;
+  font-size: 20px;
+  
+}
+
+.btn-counter {
+  display: flex;
+  margin-top: 150px;
+  justify-content: center;
+  width: 8rem;
+  position: fixed;
+}
+</style>
