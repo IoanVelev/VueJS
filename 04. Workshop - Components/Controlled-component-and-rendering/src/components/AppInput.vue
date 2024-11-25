e7f3ee2da512756b57d274094969ebc72d653587
Ioan Velev <ioanvelev46@gmail.com>
2024-11-25 23:37:18 +0200

    Create AppInput.vue

---
+++ 04. Workshop - Components/Controlled-component-and-rendering/src/components/AppInput.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  emits: ['change'],
+  data() {
+    return {
+      inputText: '',
+
+    };
+  },
+  methods: {
+    onChange(event) {
+      const text = event.target.value;
+      this.inputText = text;
+      this.$emit('change', text);
+    },
+  },
+};
+</script>
+
+<template>
+  <article class="appArticle">
+    <input type="text" @input="onChange">
+  </article>
+</template>
+
+<style>
+.appArticle {
+    max-width: 400px;
+    margin: 2rem;
+}
+</style>
