73f8285e3b30a08f1e0d68d1f94001a8dfcb6c6c
Ioan Velev <ioanvelev46@gmail.com>
2024-11-27 23:47:56 +0200

    Create GameControls.vue

---
+++ 04. Workshop - Components/memory-game-task-v2/src/components/GameControls.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    isPlaying: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['start', 'stop', 'reset'],
+};
+</script>
+
+<template>
+  <section>
+    <div class="btns">
+      <button>Start</button>
+      <button>Pause</button>
+      <button>Reset</button>
+    </div>
+  </section>
+</template>
+
+<style>
+.btns{
+
+}
+</style>
