b0e8419bd13e2addeb6a2b833bee9f5fc7eed757
Ioan Velev <ioanvelev46@gmail.com>
2024-11-29 20:58:46 +0200

    Add timer
    Timer begins when start button is clicked.

--- 04. Workshop - Components/memory-game-task-v2/src/components/GameControls.vue
+++ 04. Workshop - Components/memory-game-task-v2/src/components/GameControls.vue
@@ -1,4 +1,6 @@
 <script>
+const GAME_TIME = 90;
+let interval;
 export default {
   props: {
     isPlaying: {
@@ -6,18 +8,51 @@ export default {
       required: true,
     },
   },
-  emits: ['start', 'stop', 'reset'],
+  emits: ['start', 'end', 'reset'],
+  data() {
+    return {
+      secondsLeft: null,
+      hasLost: false,
+    };
+  },
+  methods: {
+    startHandler() {
+      this.$emit('start');
+      this.secondsLeft = GAME_TIME;
+      interval = setInterval(() => {
+        if (this.secondsLeft > 0) {
+          this.secondsLeft -= 1;
+        }
+        else {
+          this.endGame();
+        }
+      }, 1000);
+    },
+    endGame() {
+      clearInterval(interval);
+      this.$emit('end');
+      this.hasLost = true;
+    },
+  },
 };
 </script>
 
 <template>
   <section>
     <div class="btns">
-      <button>Start</button>
+      <button @click="startHandler">
+        Start
+      </button>
       <button>Pause</button>
       <button>Reset</button>
     </div>
   </section>
+  <h2 v-if="secondsLeft">
+    Seconds left: {{ secondsLeft }}
+  </h2>
+  <p v-if="hasLost">
+    You lost. :/
+  </p>
 </template>
 
 <style scoped>
