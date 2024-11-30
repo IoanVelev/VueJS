693914140b3b697450c569d5c34120daf1f3a4f5
Ioan Velev <ioanvelev46@gmail.com>
2024-11-30 15:37:55 +0200

    Create Header and Footer components
    Create Header and Footer components and import them to App.vue file.

--- 04. Workshop - Components/course-project/src/App.vue
+++ 04. Workshop - Components/course-project/src/App.vue
@@ -1,13 +1,19 @@
 <script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
 import ComponentsStarter from './components/ComponentsStarter.vue';
 
 export default {
   components: {
     ComponentsStarter,
+    AppHeader,
+    AppFooter,
   },
 };
 </script>
 
 <template>
+  <AppHeader />
   <ComponentsStarter />
+  <AppFooter />
 </template>
