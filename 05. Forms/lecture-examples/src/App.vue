cadf2b4e57649555eccb725f9f7bfb6f37096024
Ioan Velev <ioanvelev46@gmail.com>
2024-12-01 21:19:29 +0200

    Create form

--- 05. Forms/lecture-examples/src/App.vue
+++ 05. Forms/lecture-examples/src/App.vue
@@ -2,22 +2,36 @@
 export default {
   data() {
     return {
-      textValue: '',
-      title: '',
+      firstName: '',
+      lastName: '',
+      message: '',
     };
   },
   methods: {
     onInput(event) {
-      this.textValue = event.target.value;
+      this.firstName = event.target.value;
     },
   },
 };
 </script>
 
 <template>
-  <input type="text" :value="textValue" @input="onInput">
-  <p>My text is: {{ textValue }}</p>
+  <article>
+    <header>Leave your feedback</header>
+    <input type="text" :value="firstName" @input="onInput">
+    <p>My first name is: {{ firstName }}</p>
 
-  <input v-model="title" type="text">
-  <p>My title is: {{ title }}</p>
+    <input v-model="lastName" type="text">
+    <p>My last name is: {{ lastName }}</p>
+
+    <textarea v-model="message" placeholder="Leave feedback.." />
+    <p>My message is: {{ message }}</p>
+  </article>
 </template>
+
+<style scoped>
+article {
+  max-width: 500px;
+  margin: 4rem auto 0;
+}
+</style>
