543fe119c2a57aa80433774bab8be3dde5ebd9ee
Ioan Velev <ioanvelev46@gmail.com>
2024-12-01 18:36:53 +0200

    Update App.vue

--- 05. Forms/lecture-examples/src/App.vue
+++ 05. Forms/lecture-examples/src/App.vue
@@ -1,9 +1,23 @@
 <script>
 export default {
-  components: {
+  data() {
+    return {
+      textValue: '',
+      title: '',
+    };
+  },
+  methods: {
+    onInput(event) {
+      this.textValue = event.target.value;
+    },
   },
 };
 </script>
 
 <template>
+  <input type="text" :value="textValue" @input="onInput">
+  <p>My text is: {{ textValue }}</p>
+
+  <input v-model="title" type="text">
+  <p>My title is: {{ title }}</p>
 </template>
