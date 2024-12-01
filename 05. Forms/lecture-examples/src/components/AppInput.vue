b984ed3587c1e0095e282dc6b313ab788228aac3
Ioan Velev <ioanvelev46@gmail.com>
2024-12-01 22:34:52 +0200

    Create AppInput.vue

---
+++ 05. Forms/lecture-examples/src/components/AppInput.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    disabled: Boolean,
+    placeholder: String,
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <fieldset :disabled="disabled">
+    <label for="id">{{ name }}</label>
+    <input :id="id" type="text" :placeholder="placeholder">
+  </fieldset>
+</template>
