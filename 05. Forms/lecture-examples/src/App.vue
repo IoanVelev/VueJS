29621f7d27aeb8e1a959d0d0784f355730b2071d
Ioan Velev <ioanvelev46@gmail.com>
2024-12-01 22:20:40 +0200

    Update App.vue

--- 05. Forms/lecture-examples/src/App.vue
+++ 05. Forms/lecture-examples/src/App.vue
@@ -5,6 +5,11 @@ export default {
       firstName: '',
       lastName: '',
       message: '',
+      frameworks: [],
+      acceptedTerms: 'Accept',
+      car: '',
+      isSelected: false,
+      wasToggled: 'yes',
     };
   },
   methods: {
@@ -26,6 +31,71 @@ export default {
 
     <textarea v-model="message" placeholder="Leave feedback.." />
     <p>My message is: {{ message }}</p>
+
+    <fieldset>
+      <p>Select your favourite JS framework</p>
+      <label>
+        <input v-model="frameworks" type="checkbox" value="Vue">
+        VueJS
+      </label>
+      <label>
+        <input v-model="frameworks" type="checkbox" value="React">
+        ReactJS
+      </label>
+      <label>
+        <input v-model="frameworks" type="checkbox" value="Angular">
+        Angular
+      </label>
+
+      <p>Selected frameworks: {{ frameworks }}</p>
+    </fieldset>
+
+    <fieldset>
+      Please accept the website terms.
+      <label>
+        <input v-model="acceptedTerms" type="radio" value="Accept">
+        Accept
+      </label>
+      <label>
+        <input v-model="acceptedTerms" type="radio" value="Decline">
+        Decline
+      </label>
+      <p>Chosen value: {{ acceptedTerms }} </p>
+    </fieldset>
+
+    <fieldset>
+      <select v-model="car">
+        <option disabled value="">
+          Please select car manufacturer
+        </option>
+        <option>Toyota</option>
+        <option>Ford</option>
+        <option>Mercedes</option>
+      </select>
+
+      <p>Selected car: {{ car }}</p>
+    </fieldset>
+
+    <fieldset>
+      <label>
+        <input
+          v-model="wasToggled"
+          type="checkbox"
+          true-value="yes"
+          false-value="no"
+        >
+        Was I toggled?
+        <span>{{ wasToggled }}</span>
+      </label>
+      <label>
+        <input
+          v-model="isSelected"
+          type="checkbox"
+        >
+        Was I selected?
+        <span>{{ isSelected }}</span>
+      </label>
+    </fieldset>
   </article>
 </template>
 
@@ -33,5 +103,9 @@ export default {
 article {
   max-width: 500px;
   margin: 4rem auto 0;
+  fieldset {
+    border: 1px lightcoral solid;
+    padding: 10px;
+  }
 }
 </style>
