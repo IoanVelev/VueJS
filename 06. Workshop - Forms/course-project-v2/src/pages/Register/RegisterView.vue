c3f6d4b66e629dfc1b04de23e48e4f40dee78c70
Ioan Velev <ioanvelev46@gmail.com>
2024-12-05 17:22:19 +0200

    Display register form

--- 06. Workshop - Forms/course-project-v2/src/pages/Register/RegisterView.vue
+++ 06. Workshop - Forms/course-project-v2/src/pages/Register/RegisterView.vue
@@ -1,3 +1,14 @@
+<script>
+import GeneralForm from './components/GeneralForm.vue';
+
+export default {
+  components: {
+    GeneralForm,
+  },
+};
+</script>
+
 <template>
   <h1>Registration page</h1>
+  <GeneralForm />
 </template>
