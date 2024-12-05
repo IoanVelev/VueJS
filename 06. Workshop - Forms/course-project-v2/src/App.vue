0eb028c34acd18af064b2932d5f67a47b33681a7
Ioan Velev <ioanvelev46@gmail.com>
2024-12-05 16:08:45 +0200

    Add resources

---
+++ 06. Workshop - Forms/course-project-v2/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AboutView from './pages/AboutView.vue';
+import CartView from './pages/CartView.vue';
+import ContactsView from './pages/ContactsView.vue';
+import HomeView from './pages/HomeView.vue';
+import ProductsView from './pages/Products/ProductsView.vue';
+
+export default {
+  components: {
+    AppHeader,
+    ProductsView,
+    AppFooter,
+    HomeView,
+    ContactsView,
+    AboutView,
+    CartView,
+  },
+  data() {
+    return {
+      page: 'Products',
+    };
+  },
+  methods: {
+    onSelect(newPage) {
+      this.page = newPage;
+    },
+  },
+};
+</script>
+
+<template>
+  <AppHeader @select="onSelect" />
+  <!-- <ComponentsStarter /> -->
+  <main>
+    <component :is="page" />
+  </main>
+  <AppFooter />
+</template>
---
+++ 06. Workshop - Forms/course-project-v2/src/pages/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>About</h1>
+  <p>
+    This is about page.
+  </p>
+</template>
