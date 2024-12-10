db1b140ed39a9249f7db650e2551810d779119f0
Ioan Velev <ioanvelev46@gmail.com>
2024-12-10 19:24:08 +0200

    Add resources

---
+++ 07. Directives, HTTP and Routing/lecture-examples-directives/eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  vue: true,
+  typescript: false,
+}, {
+  rules: {
+    'no-console': 'off',
+    'style/semi': ['error', 'always'],
+    'vue/no-multiple-template-root': 'off',
+  },
+});
