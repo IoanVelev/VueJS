042c294352a517f6276e53a3dcb549914bef671d
Ioan Velev <ioanvelev46@gmail.com>
2024-12-10 19:24:28 +0200

    Create directive

---
+++ 07. Directives, HTTP and Routing/lecture-examples-directives/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+const focus = {
+mounted: (element) => {
+element.focus()
+}
+}
+
+export default {
+directives: {
+  focus
+}
+}
+</script>
+
+<template>
+  <input v-focus>
+</template>
+
+<style>
+
+</style>
