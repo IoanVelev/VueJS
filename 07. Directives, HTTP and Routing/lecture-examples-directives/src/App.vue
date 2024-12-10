1bd7f706bbe9f680b984229eed558b3c6b22816d
Ioan Velev <ioanvelev46@gmail.com>
2024-12-10 22:19:12 +0200

    Use custom directives for DOM manipulation

--- 07. Directives, HTTP and Routing/lecture-examples-directives/src/App.vue
+++ 07. Directives, HTTP and Routing/lecture-examples-directives/src/App.vue
@@ -9,12 +9,24 @@ const focus = {
     if (bindingData.value) {
       element.focus();
     }
-  }
+  },
+};
+
+const colorDir = {
+  mounted: (el, binding) => {
+    if (binding.arg === "background") {
+      // set background color
+      el.style.backgroundColor = binding.value;
+    } else {
+      el.style.color = binding.value;
+    }
+  },
 };
 
 export default {
   directives: {
     focus,
+    color: colorDir,
   },
   data() {
     return {
@@ -27,6 +39,9 @@ export default {
 <template>
   <button @click="shouldFocus = !shouldFocus">Toggle {{ shouldFocus }}</button>
   <input v-focus:foo.trim.prevent="shouldFocus" />
+
+  <p v-color="'red'">I am using v-color with text</p>
+  <p v-color:background="'green'">I am using v-color with background</p>
 </template>
 
 <style></style>
