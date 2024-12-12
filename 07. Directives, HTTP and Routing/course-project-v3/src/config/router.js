331c14ab1ff4c50736980a34e04b267b63f9a4e8
Ioan Velev <ioanvelev46@gmail.com>
2024-12-12 22:34:27 +0200

    Add new route with nested routes

--- 07. Directives, HTTP and Routing/course-project-v3/src/config/router.js
+++ 07. Directives, HTTP and Routing/course-project-v3/src/config/router.js
@@ -3,9 +3,14 @@ import AboutView from '../pages/AboutView.vue';
 import CartView from '../pages/CartView.vue';
 import ContactsView from '../pages/ContactsView.vue';
 import HomeView from '../pages/HomeView.vue';
+import NotFound from '../pages/NotFound.vue';
 import ProductsView from '../pages/Products/ProductsView.vue';
 import SingleProduct from '../pages/Products/SingleProduct.vue';
 import RegisterView from '../pages/Register/RegisterView.vue';
+import User from '../pages/User/User.vue';
+import UserDetails from '../pages/User/UserDetails.vue';
+import UserEdit from '../pages/User/UserEdit.vue';
+import UserHome from '../pages/User/UserHome.vue';
 
 const routes = [
   { path: '/', component: HomeView },
@@ -15,6 +20,17 @@ const routes = [
   { path: '/contacts', component: ContactsView },
   { path: '/product/:id', component: SingleProduct },
   { path: '/cart', component: CartView },
+  {
+    path: '/user',
+    component: User,
+    children: [
+      { path: '', component: UserHome },
+      { path: ':id', component: UserDetails },
+      { path: ':id/edit', component: UserEdit },
+    ],
+  },
+
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
