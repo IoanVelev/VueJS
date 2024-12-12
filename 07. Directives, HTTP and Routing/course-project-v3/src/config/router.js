edb114794b90d560b6e2a20266927bb0eec73b93
Ioan Velev <ioanvelev46@gmail.com>
2024-12-12 19:56:43 +0200

    Update router.js

--- 07. Directives, HTTP and Routing/course-project-v3/src/config/router.js
+++ 07. Directives, HTTP and Routing/course-project-v3/src/config/router.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AboutView from '../pages/AboutView.vue';
+import CartView from '../pages/CartView.vue';
 import ContactsView from '../pages/ContactsView.vue';
 import HomeView from '../pages/HomeView.vue';
+import ProductsView from '../pages/Products/ProductsView.vue';
 import RegisterView from '../pages/Register/RegisterView.vue';
+import SingleProduct from '../pages/SingleProduct.vue';
 
 const routes = [
   { path: '/', component: HomeView },
+  { path: '/products', component: ProductsView },
   { path: '/about', component: AboutView },
   { path: '/regsiter', component: RegisterView },
   { path: '/contacts', component: ContactsView },
+  { path: '/product/:id', component: SingleProduct },
+  { path: '/cart', component: CartView },
 ];
 
 const router = createRouter({
