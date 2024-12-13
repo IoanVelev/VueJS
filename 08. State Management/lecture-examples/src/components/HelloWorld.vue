92a95b4582191f9444992003a05537fec5fbdb5b
Ioan Velev <ioanvelev46@gmail.com>
2024-12-13 16:55:16 +0200

    Update HelloWorld.vue

--- 08. State Management/lecture-examples/src/components/HelloWorld.vue
+++ 08. State Management/lecture-examples/src/components/HelloWorld.vue
@@ -1,11 +1,16 @@
-<script setup>
-import { ref } from 'vue';
+<script>
+import { useCountStore } from '../stores/useCountStore';
+import DoubledCount from './doubledCount.vue';
 
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+export default {
+  components: {
+    DoubledCount,
+  },
+  setup() {
+    const countStore = useCountStore();
+    return { countStore };
+  },
+};
 </script>
 
 <template>
@@ -14,28 +19,12 @@ const count = ref(0);
     <h1>{{ msg }}</h1>
 
     <div class="card">
-      <button type="button" @click="count++">
-        count is {{ count }}
+      <button type="button" @click="countStore.increment">
+        count is {{ countStore.count }}
       </button>
-      <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test HMR
-      </p>
-    </div>
 
-    <p>
-      Check out
-      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
-      starter
-    </p>
-    <p>
-      Install
-      <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-      in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">
-      Click on the Vite and Vue logos to learn more
-    </p>
+      <DoubledCount />
+    </div>
   </div>
 </template>
 
