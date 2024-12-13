5758d88d119aaa4edc7f41004cb2b5ec7e09f1fd
Ioan Velev <ioanvelev46@gmail.com>
2024-12-13 16:55:20 +0200

    Create doubledCount.vue

---
+++ 08. State Management/lecture-examples/src/components/doubledCount.vue
@@ -0,0 +1,14 @@
+<script>
+import { useCountStore } from '../stores/useCountStore';
+
+export default {
+  setup() {
+    const countStore = useCountStore();
+    return { countStore };
+  },
+};
+</script>
+
+<template>
+  <h2>Doubled count is: {{ countStore.doubledCount }}</h2>
+</template>
