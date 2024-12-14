b41ab3651f8c84701efc92c4a91dcebc9bc95684
Ioan Velev <ioanvelev46@gmail.com>
2024-12-14 23:19:40 +0200

    Update products.js

--- 08. State Management/course-project-v4/src/services/products.js
+++ 08. State Management/course-project-v4/src/services/products.js
@@ -25,3 +25,19 @@ export async function getProductsByCategory(selectedCategory) {
     return [];
   }
 }
+
+export async function getProductsByIds(ids) {
+  if (!Array.isArray(ids))
+    return;
+  try {
+    const promises = ids.map(id => axiosInstance.get(`/${ENDPOINT}/category/${id}`));
+    const response = await Promise.allSettled(promises);
+    return response
+      .filter(entry => entry.status === 'fulfilled')
+      .map(entry => entry.value);
+  }
+  catch (e) {
+    console.error('An error occurred', e);
+    return [];
+  }
+}
