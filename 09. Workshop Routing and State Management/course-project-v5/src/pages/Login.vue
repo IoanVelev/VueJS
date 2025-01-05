9739b995ad6ffb8dc8f724a3b301587e148dbdba
Ioan Velev <ioanvelev46@gmail.com>
2025-01-05 17:26:45 +0200

    Create Login.vue

---
+++ 09. Workshop Routing and State Management/course-project-v5/src/pages/Login.vue
@@ -0,0 +1,61 @@
+<script>
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
+import FormFieldset from './Register/components/FormFieldset.vue';
+
+export default {
+  components: {
+    FormFieldset,
+  },
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  validations() {
+    return {
+      form: {
+        username: { required },
+        password: { required },
+      },
+    };
+  },
+  methods: {
+    async onLogin() {
+      const isValid = await this.v$.$validate();
+
+      if (!isValid)
+        return;
+
+      console.log(this.form);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <article>
+      <form @submit.prevent="onLogin">
+        <FormFieldset title="Username" :errors="v$.form.password.$errors">
+          <input v-model="v$.form.username.$model" type="password" placeholder="Enter your username">
+        </FormFieldset>
+        <FormFieldset title="password" :errors="v$.form.rePass.$errors">
+          <input v-model="v$.form.password.$model" type="password" placeholder="Enter your password">
+        </FormFieldset>
+
+        <button class="primary" type="submit">
+          Login
+        </button>
+      </form>
+    </article>
+  </div>
+</template>
